d5483290bdce0e7e93d03ddd35b0d8b8d6528c41
inaumanmajeed <inaumanmajeed@gmail.com>
2024-05-12 03:23:39 +0500

    responsive

--- todo-redux/src/components/todo/TodoCard.js
+++ todo-redux/src/components/todo/TodoCard.js
@@ -8,12 +8,14 @@ const TodoCard = () => {
   return (
     <>
       <div className="main__container">
+      <div className="squares">
         <div className="square" style={{ "--i": 0 }}></div>
         <div className="square" style={{ "--i": 1 }}></div>
         <div className="square" style={{ "--i": 2 }}></div>
         <div className="square" style={{ "--i": 3 }}></div>
         <div className="square" style={{ "--i": 4 }}></div>
         <div className="square" style={{ "--i": 5 }}></div>
+        </div>
         <div className="container__content">
           <AddTodo />
           <h2 style={{textAlign: 'center'}}>TodoList</h2>
