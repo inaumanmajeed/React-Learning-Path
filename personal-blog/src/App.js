7faa070d0d520dd56802f449e01a21dcb9af5418
inaumanmajeed <inaumanmajeed@gmail.com>
2024-05-17 00:50:58 +0500

    chore: Update App.js to handle user authentication and display loading state

--- personal-blog/src/App.js
+++ personal-blog/src/App.js
@@ -1,10 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/slicers/authSlice";
+import { Footer, Header } from "./components";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>Personal Blog</h1>
-    </>
+  useEffect(() => {
+    authService
+      .getUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div>
+      <div>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    "Loading..."
   );
 }
 
